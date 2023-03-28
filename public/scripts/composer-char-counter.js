52671b88e245f2faea9960d915456ca55c5c7d5c
stanw0ng <user.stanley@gmail.com>
2023-03-28 18:06:08 -0400

    renamed counter variable for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  const counter = $('.counter'); // Cache the counter element outside the listener, search is made only once
-  const originalCharCount = parseInt(counter.text()); // Store the original character count as a number
+  const $counter = $('.counter'); // Cache the counter element outside the listener, search is made only once
+  const originalCharCount = parseInt($counter.text()); // Store the original character count as a number
 
   $('#tweet-text').on('input', function(event) {
     const tweetLength = $(this).val().length; // this happens multiple times
     const charCount = originalCharCount - tweetLength;
-    counter.text(charCount);
+    $counter.text(charCount);
     
     if(charCount < 0) {
-      counter.css('color', '#FF0000')
+      $counter.css('color', '#FF0000')
     } else {
-      counter.css('color', '#545149')
+      $counter.css('color', '#545149')
     }
   });
 });
