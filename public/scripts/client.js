0d13aafc2afc2d84815ccd1943799982c2095908
stanw0ng <user.stanley@gmail.com>
2023-03-29 13:25:21 -0400

    added function in client.js to render multiple tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,66 @@
 $(document).ready(function() {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  // DATABASE
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const createTweetElement = (tweetData) => {
-
-  const userTweet = 
-
-  `<article class="article">
-  <header class="tweet-header">
+  // creates tweet element by taking in data and formats it to a template
+  const createTweetElement = (tweet) => {
+    
+    const userTweet = 
+    
+    `<article class="article">
+    <header class="tweet-header">
     <div class="user-logo">
-      <img class="avatar" src="${tweetData.user.avatars}">
-      <span class="display-name">${tweetData.user.name}</span>
+    <img class="avatar" src="${tweet.user.avatars}">
+    <span class="display-name">${tweet.user.name}</span>
     </div>
-    <span class="handle">${tweetData.user.handle}</span>
-  </header>
-  <p class="tweet-content">${tweetData.content.text}</p>
-  <footer class="tweet-footer">
-    <span class="timestamp">Date Posted</span>
+    <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <p class="tweet-content">${tweet.content.text}</p>
+    <footer class="tweet-footer">
+    <span class="timestamp">${tweet.created_at}</span>
     <span class="engage-icons">
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
     </span>
-  </footer>
-</article>`;
+    </footer>
+    </article>`;
+    
+    return userTweet;
+  }
+  
+  // renders each tweet
+  const renderTweets = function(tweets) {
+    for (user in tweets) {
+      const $tweet = createTweetElement(tweets[user]);
+      $('#tweets-container').append($tweet);
+    }
+  }
 
-return userTweet;
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 
 });
